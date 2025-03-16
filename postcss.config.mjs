bada0080dab57bab4a8b2f1826d17c34837b0790
Nguyễn Thái Son Ny <134358623+sonnynguyenthai@users.noreply.github.com>
2025-03-16 16:29:11 +1100

    feature/init-config-base-project

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,23 @@
+/** @type {import('tailwindcss').Config} */
 const config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E3A8A",  // Custom primary color
+        secondary: "#9333EA", // Custom secondary color
+        accent: {
+          light: "#FDE68A",
+          DEFAULT: "#F59E0B",
+          dark: "#B45309",
+        },
+      },
+    },
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
